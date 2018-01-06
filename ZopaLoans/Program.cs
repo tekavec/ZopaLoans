7b19b1652ff94e40bf6bf3514352407ec6fc2faf
tekavec <tomaz.tekavec@gmail.com>
2018-01-06 00:01:55 +0000

    Add project files.

---
+++ ZopaLoans/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZopaLoans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
